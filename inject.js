f53c95793be48e54cb31a821a159f0c0fbb04b0d
Carlos Toxtli <ctoxtli@gmail.com>
2018-03-10 23:54:35 -0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -2,4 +2,5 @@ var s = document.createElement('script');
 s.type = 'text/javascript';
 s.src = '//www.carlostoxtli.com/github-pages-cms/raptor.js';
 (top.document.body || top.document.getElementsByTagName('head')[0]).appendChild(s);
+$('body').raptor();
 alert('Code injected');
