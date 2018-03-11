9debb737b542bb54665d195b90b062e2e5b5de37
Carlos Toxtli <ctoxtli@gmail.com>
2018-03-11 00:02:16 -0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,6 +1,21 @@
-var s = document.createElement('script');
-s.type = 'text/javascript';
-s.src = '//www.carlostoxtli.com/github-pages-cms/raptor.js';
-(top.document.body || top.document.getElementsByTagName('head')[0]).appendChild(s);
-$('body').raptor();
+function loadLibrary(url) {  
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.src = url;
+  (top.document.body || top.document.getElementsByTagName('head')[0]).appendChild(s);
+}
+
+function loadLibraries() {
+  loadLibrary('//www.carlostoxtli.com/github-pages-cms/raptor.js');
+  if(typeof jQuery=='undefined') {
+    loadLibrary('//code.jquery.com/jquery-3.3.1.js');
+  }
+}
+
+function init() {
+  $('body').raptor();
+}
+
+loadLibraries();
+init();
 alert('Code injected');
